d60c73fb941abd21eb07f79e5bdbdb556bd870cb
Kübra <soysalkubra1@icloud.com>
2025-08-14 11:47:40 +0300

    silindi

--- android/app/src/main/kotlin/com/example/restaurant/restaurant_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.restaurant.restaurant_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/config.dart
+++
@@ -1,1 +0,0 @@
-const String baseUrl = 'http://192.168.232.113:8080';
